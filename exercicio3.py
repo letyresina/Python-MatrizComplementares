ded542ccbe6eb001119c55358d277515d9376814
letyresina <leticiaresina@gmail.com>
2023-09-26 10:33:28 -0300

    terminar em casa

---
+++ exercicio3.py
@@ -0,0 +1,16 @@
+'''
+    Exercício 3:
+    Faça um programa que preenche uma matriz 7x7 com números aleatórios e gere uma lista de 7 elementos 
+    que contenha o maior elemento de cada uma das linhas da matriz.
+'''
+
+# Importações
+
+from moduloMatriz import exibeMatriz
+
+from random import randint
+
+# Criando matriz
+matriz = []
+
+
