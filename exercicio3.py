b82e07deb4b8668dff2a13e086ebcd2f52fb8a09
letyresina <leticiaresina@gmail.com>
2023-09-29 10:58:50 -0300

    Subindo exercício 3

--- exercicio3.py
+++ exercicio3.py
@@ -13,4 +13,22 @@ from random import randint
 # Criando matriz
 matriz = []
 
+# Lista de maiores números
+maioresNumeros = []
 
+for linha in range(7):
+   lista = []
+   maiorNumero = None  # Definir maiorNumero para None a cada nova linha
+   for coluna in range(7):
+       num = randint(1, 50)
+       lista.append(num)
+       if maiorNumero is None or num > maiorNumero:
+           maiorNumero = num
+   maioresNumeros.append(maiorNumero)  # Adicionar o maior número da linha à lista
+   matriz.append(lista)
+        
+
+
+exibeMatriz(matriz)
+
+print(maioresNumeros)
