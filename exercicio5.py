5a5af6214bbb8e8186ec5ffd1eef5d3057f968d5
letyresina <leticiaresina@gmail.com>
2023-09-29 11:46:43 -0300

    terminar exercício 5

--- exercicio5.py
+++ exercicio5.py
@@ -5,3 +5,26 @@
     preencha uma matriz 4x4 com valores informados pelo usuário e informe se ela é ou não um quadrado 
     mágico.
 '''
+
+# Importações
+
+from moduloMatriz import exibeMatriz
+
+# Criando matriz
+matriz = []
+
+for linha in range(4):
+    lista = []
+    for coluna in range(4):
+        num = int(input("Digite um número inteiro: "))
+        lista.append(num)
+    matriz.append(lista)
+
+# percorrer indices da matriz
+for i in range(len(matriz)):
+    for j in range(len(matriz[0])):
+        if sum(matriz[i][j]) == matriz[i][j] * matriz[i][j]:
+            print("TESTE")
+           
+
+exibeMatriz(matriz)
